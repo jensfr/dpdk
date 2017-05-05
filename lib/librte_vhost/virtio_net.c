2ad68b1b09ec01802d06084dfc9d7144a79f50bd
Yuanhan Liu <yuanhan.liu@linux.intel.com>
2017-05-05 09:57:18 -0400

    vhost: mark desc being used
    Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

--- lib/librte_vhost/virtio_net.c
+++ lib/librte_vhost/virtio_net.c
@@ -1192,6 +1192,7 @@ dequeue_desc(struct virtio_net *dev, struct vhost_virtqueue *vq,
 	 */
 	if (likely((desc->len == dev->vhost_hlen) &&
 		   (desc->flags & VRING_DESC_F_NEXT) != 0)) {
+		rte_smp_wmb();
 		desc->flags = 0;
 
 		desc = &descs[(head_idx++) & (vq->size - 1)];
@@ -1251,11 +1252,11 @@ dequeue_desc(struct virtio_net *dev, struct vhost_virtqueue *vq,
 
 		/* This desc reaches to its end, get the next one */
 		if (desc_avail == 0) {
-			desc->flags = 0;
-
 			if ((desc->flags & VRING_DESC_F_NEXT) == 0)
 				break;
 
+			rte_smp_wmb();
+			desc->flags = 0;
 			desc = &descs[(head_idx++) & (vq->size - 1)];
 			if (unlikely(desc->flags & VRING_DESC_F_INDIRECT))
 				return -1;
@@ -1294,6 +1295,7 @@ dequeue_desc(struct virtio_net *dev, struct vhost_virtqueue *vq,
 			mbuf_avail  = cur->buf_len - RTE_PKTMBUF_HEADROOM;
 		}
 	}
+	rte_smp_wmb();
 	desc->flags = 0;
 
 	prev->data_len = mbuf_offset;
