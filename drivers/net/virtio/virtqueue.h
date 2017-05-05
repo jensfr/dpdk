cd05db0cbff80bd13c895aa762456560a59c4163
Yuanhan Liu <yuanhan.liu@linux.intel.com>
2017-05-05 09:57:12 -0400

    net/virtio: vring init for 1.1
    Add and initialize descriptor data structures.
    
    Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>
    [rename desc_1_1 to vring_desc_1_1, refactor desc init code]
    Signed-off-by: Jens Freimann <jfreiman@redhat.com>

--- drivers/net/virtio/virtqueue.h
+++ drivers/net/virtio/virtqueue.h
@@ -265,6 +265,16 @@ struct virtio_tx_region {
 			   __attribute__((__aligned__(16)));
 };
 
+static inline void
+vring_desc_init_1_1(struct vring *vr, int n)
+{
+	int i;
+	for (i = 0; i < n; i++) {
+		struct vring_desc_1_1 *desc = &vr->desc_1_1[i];
+		desc->index = i;
+	}
+}
+
 /* Chain all the descriptors in the ring with an END */
 static inline void
 vring_desc_init(struct vring_desc *dp, uint16_t n)
