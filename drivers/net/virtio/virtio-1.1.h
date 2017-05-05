4b5da92cfdb38df4a829501e0c0d15f30e3bc20b
Yuanhan Liu <yuanhan.liu@linux.intel.com>
2017-05-05 09:57:12 -0400

    net/virtio: vring init for 1.1
    Add and initialize descriptor data structures.
    
    Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>
    [rename desc_1_1 to vring_desc_1_1, refactor desc init code, add
    DESC_DRIVER and DESC_WRAP flags]
    Signed-off-by: Jens Freimann <jfreiman@redhat.com>

---
+++ drivers/net/virtio/virtio-1.1.h
@@ -0,0 +1,20 @@
+#ifndef __VIRTIO_1_1_H
+#define __VIRTIO_1_1_H
+
+#define VRING_DESC_F_NEXT	1
+#define VRING_DESC_F_WRITE	2
+#define VRING_DESC_F_INDIRECT	4
+
+#define BATCH_NOT_FIRST 0x0010
+#define BATCH_NOT_LAST  0x0020
+#define DESC_DRIVER	0x0080
+#define DESC_WRAP	0x0100
+
+struct vring_desc_1_1 {
+        uint64_t addr;
+        uint32_t len;
+        uint16_t index;
+        uint16_t flags;
+};
+
+#endif /* __VIRTIO_1_1_H */
