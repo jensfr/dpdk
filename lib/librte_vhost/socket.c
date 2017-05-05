3c54fe22c24f8f26a3d0ae47ee567450bed86b7b
Yuanhan Liu <yuanhan.liu@linux.intel.com>
2017-05-05 09:57:15 -0400

    vhost: enable packed virtqueues
    This patch enables the code do enqueue and dequeue packed to/from a
    packed virtqueue.  Add feature bit for packed virtqueues as defined in
    Virtio 1.1 draft.
    
    Signed-off-by: Jens Freimann <jfreimann@redhat.com>
    Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

--- lib/librte_vhost/socket.c
+++ lib/librte_vhost/socket.c
@@ -850,6 +850,11 @@ rte_vhost_driver_register(const char *path, uint64_t flags)
 		vsocket->features &= ~(1ULL << VIRTIO_F_IOMMU_PLATFORM);
 	}
 
+#ifdef RTE_LIBRTE_VHOST_PQ
+		vsocket->features |= (1ULL << VIRTIO_F_RING_PACKED);
+		vsocket->features &= ~(1ULL << VIRTIO_RING_F_INDIRECT_DESC);
+#endif
+
 	if ((flags & RTE_VHOST_USER_CLIENT) != 0) {
 		vsocket->reconnect = !(flags & RTE_VHOST_USER_NO_RECONNECT);
 		if (vsocket->reconnect && reconn_tid == 0) {
