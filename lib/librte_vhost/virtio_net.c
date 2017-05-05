d60cb8ec7ccbcaa1412d8dcc5f0c04c1fd8c1e0a
Yuanhan Liu <yuanhan.liu@linux.intel.com>
2017-05-05 09:57:15 -0400

    vhost: enable packed virtqueues
    This patch enables the code do enqueue and dequeue packed to/from a
    packed virtqueue.  Add feature bit for packed virtqueues as defined in
    Virtio 1.1 draft.
    
    Signed-off-by: Jens Freimann <jfreimann@redhat.com>
    Signed-off-by: Yuanhan Liu <yuanhan.liu@linux.intel.com>

--- lib/librte_vhost/virtio_net.c
+++ lib/librte_vhost/virtio_net.c
@@ -841,7 +841,9 @@ rte_vhost_enqueue_burst(int vid, uint16_t queue_id,
 		return 0;
 	}
 
-	if (dev->features & (1 << VIRTIO_NET_F_MRG_RXBUF))
+	if (dev->features & (1ULL << VIRTIO_F_RING_PACKED))
+		return vhost_enqueue_burst_packed(dev, queue_id, pkts, count);
+	else if (dev->features & (1 << VIRTIO_NET_F_MRG_RXBUF))
 		return virtio_dev_merge_rx(dev, queue_id, pkts, count);
 	else
 		return virtio_dev_rx(dev, queue_id, pkts, count);
@@ -1511,6 +1513,10 @@ rte_vhost_dequeue_burst(int vid, uint16_t queue_id,
 
 	if (unlikely(vq->enabled == 0))
 		goto out_access_unlock;
+
+	if (dev->features & (1ULL << VIRTIO_F_RING_PACKED))
+		return vhost_dequeue_burst_packed(dev, vq, mbuf_pool, pkts,
+						  count);
 
 	vq->batch_copy_nb_elems = 0;
 
