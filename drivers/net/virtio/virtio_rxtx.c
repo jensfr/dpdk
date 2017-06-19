33806231605d783e713c222042af6403bcbe9fdc
Tiwei Bie <tiwei.bie@intel.com>
2017-06-19 12:45:00 +0800

    net/virtio: avoid touching packet data
    For performance testing purpose, avoid touching the packet data
    when receiving packets.
    
    Signed-off-by: Tiwei Bie <tiwei.bie@intel.com>

--- drivers/net/virtio/virtio_rxtx.c
+++ drivers/net/virtio/virtio_rxtx.c
@@ -137,7 +137,9 @@ virtqueue_dequeue_burst_rx_1_0(struct virtqueue *vq, struct rte_mbuf **rx_pkts,
 		}
 
 		rte_prefetch0(cookie);
+#if 0
 		rte_packet_prefetch(rte_pktmbuf_mtod(cookie, void *));
+#endif
 		rx_pkts[i]  = cookie;
 		vq->vq_used_cons_idx++;
 		vq_ring_free_chain(vq, desc_idx);
@@ -172,7 +174,9 @@ virtqueue_dequeue_burst_rx_1_1(struct virtqueue *vq, struct rte_mbuf **rx_pkts,
 		vq->vq_descx[used_idx].cookie = NULL;
 
 		rte_prefetch0(cookie);
+#if 0
 		rte_packet_prefetch(rte_pktmbuf_mtod(cookie, void *));
+#endif
 		rx_pkts[i]  = cookie;
 
 		vq->vq_used_cons_idx++;
@@ -563,7 +567,9 @@ static void
 virtio_update_packet_stats(struct virtnet_stats *stats, struct rte_mbuf *mbuf)
 {
 	uint32_t s = mbuf->pkt_len;
+#if 0
 	struct ether_addr *ea;
+#endif
 
 	if (s == 64) {
 		stats->size_bins[1]++;
@@ -582,6 +588,7 @@ virtio_update_packet_stats(struct virtnet_stats *stats, struct rte_mbuf *mbuf)
 			stats->size_bins[7]++;
 	}
 
+#if 0
 	ea = rte_pktmbuf_mtod(mbuf, struct ether_addr *);
 	if (is_multicast_ether_addr(ea)) {
 		if (is_broadcast_ether_addr(ea))
@@ -589,6 +596,7 @@ virtio_update_packet_stats(struct virtnet_stats *stats, struct rte_mbuf *mbuf)
 		else
 			stats->multicast++;
 	}
+#endif
 }
 
 /* Optionally fill offload information in structure */
