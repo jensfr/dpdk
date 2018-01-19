999e60d333a838bff41532c01d3ee38508c0a65f
Jens Freimann <jfreimann@redhat.com>
2018-01-19 11:26:57 +0100

    vhost: add virtio 1.1 defines
    This should actually be in the kernel header file, but it isn't
    yet. For now let's use our own headers.
    
    Signed-off-by: Jens Freimann <jfreimann@redhat.com>

---
+++ lib/librte_vhost/virtio-1.1.h
@@ -0,0 +1,18 @@
+#ifndef __VIRTIO_PACKED_H
+#define __VIRTIO_PACKED_H
+
+#define VRING_DESC_F_NEXT       1
+#define VRING_DESC_F_WRITE      2
+#define VRING_DESC_F_INDIRECT   4
+
+#define VRING_DESC_F_AVAIL      (1ULL << 7)
+#define VRING_DESC_F_USED	(1ULL << 15)
+
+struct vring_desc_packed {
+	uint64_t addr;
+	uint32_t len;
+	uint16_t index;
+	uint16_t flags;
+};
+
+#endif /* __VIRTIO_PACKED_H */
