1402ab65e0d0398fb36c694780ca2b66d5b4c998
Jens Freimann <jfreimann@redhat.com>
2018-01-19 11:26:57 +0100

    vhost: add virtio 1.1 defines
    This should actually be in the kernel header file, but it isn't
    yet. For now let's use our own headers.
    
    Signed-off-by: Jens Freimann <jfreimann@redhat.com>

--- lib/librte_vhost/vhost.h
+++ lib/librte_vhost/vhost.h
@@ -167,7 +167,9 @@ struct vhost_msg {
 #ifndef VIRTIO_F_VERSION_1
  #define VIRTIO_F_VERSION_1 32
 #endif
-
+#ifndef VIRTIO_F_PACKED_RING
+ #define VIRTIO_F_PACKED_RING 34
+#endif
 #define VHOST_USER_F_PROTOCOL_FEATURES	30
 
 /* Features supported by this builtin vhost-user net driver. */
