5780efee053dc930df30cd0fc7f0fb670f57c945
Jens Freimann <jfreimann@redhat.com>
2018-04-03 11:54:12 -0400

    vhost: disable packed virtqueues by default
    Signed-off-by: Jens Freimann <jfreimann@redhat.com>

--- lib/librte_vhost/socket.c
+++ lib/librte_vhost/socket.c
@@ -670,6 +670,10 @@ rte_vhost_driver_register(const char *path, uint64_t flags)
 		vsocket->features &= ~(1ULL << VIRTIO_F_IOMMU_PLATFORM);
 	}
 
+#ifndef RTE_LIBRTE_VHOST_PQ
+		vsocket->features &= ~(1ULL << VIRTIO_F_RING_PACKED);
+#endif
+
 	if ((flags & RTE_VHOST_USER_CLIENT) != 0) {
 		vsocket->reconnect = !(flags & RTE_VHOST_USER_NO_RECONNECT);
 		if (vsocket->reconnect && reconn_tid == 0) {
