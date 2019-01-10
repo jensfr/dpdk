f0d63d1c5cea1d19bec530b38d01a8ace6d647e1
Jens Freimann <jfreimann@redhat.com>
2019-01-10 10:55:22 +0100

    net/virtio-user: ctrl vq support for packed
    Add support to virtio-user for control virtqueues.
    
    Signed-off-by: Jens Freimann <jfreimann@redhat.com>

--- drivers/net/virtio/virtio_user/virtio_user_dev.h
+++ drivers/net/virtio/virtio_user/virtio_user_dev.h
@@ -39,7 +39,11 @@ struct virtio_user_dev {
 	uint16_t	port_id;
 	uint8_t		mac_addr[ETHER_ADDR_LEN];
 	char		path[PATH_MAX];
-	struct vring	vrings[VIRTIO_MAX_VIRTQUEUES];
+	union {
+		struct vring		vrings[VIRTIO_MAX_VIRTQUEUES];
+		struct vring_packed	packed_vrings[VIRTIO_MAX_VIRTQUEUES];
+	};
+
 	struct virtio_user_backend_ops *ops;
 	pthread_mutex_t	mutex;
 	bool		started;
@@ -53,5 +57,7 @@ int virtio_user_dev_init(struct virtio_user_dev *dev, char *path, int queues,
 			 int mrg_rxbuf, int in_order, int packed_vq);
 void virtio_user_dev_uninit(struct virtio_user_dev *dev);
 void virtio_user_handle_cq(struct virtio_user_dev *dev, uint16_t queue_idx);
+void virtio_user_handle_cq_packed(struct virtio_user_dev *dev,
+		uint16_t queue_idx, struct virtqueue *vq);
 uint8_t virtio_user_handle_mq(struct virtio_user_dev *dev, uint16_t q_pairs);
 #endif
